95a119183fe38a8ca32e3a084e10cf6232ef6b2d
Lilah Dicker <lilahdicker@Lilahs-MacBook-Pro-2.local>
2025-09-18 17:08:55 -0400

    vite:

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "https://github.com/cs280fa24-homework/hw-02-ldicker6" /* TODO: update to your repository name */,
+  base: "/sticky-notes-app/",
 });
